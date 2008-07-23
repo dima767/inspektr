33104e508f78ff408ed55630331499273b41bf57
Scott Battaglia <scott.battaglia@gmail.com>
2008-07-23 00:23:30 +0000

    fixed package names
    git-svn-id: http://inspektr.googlecode.com/svn/trunk@86 465cf23e-ef40-0410-a6d9-5d4bcebfc45a

---
+++ inspektr-statistics/src/main/java/org/inspektr/statistics/StatisticManagementAspect.java
@@ -0,0 +1,97 @@
+/**
+ *  Copyright 2007 Rutgers, the State University of New Jersey
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *      
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.inspektr.statistics;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.inspektr.common.annotation.NotEmpty;
+import org.inspektr.common.annotation.NotNull;
+import org.inspektr.common.spi.ClientInfoResolver;
+import org.inspektr.common.spi.DefaultClientInfoResolver;
+import org.inspektr.common.web.ClientInfo;
+import org.inspektr.statistics.annotation.Statistic;
+import org.inspektr.statistics.spi.StatisticNameResolver;
+import org.inspektr.statistics.spi.support.DefaultStatisticNameResolver;
+
+/**
+ * A POJO style aspect modularizing management of a statistic data concern.
+ * 
+ * @author Scott Battaglia
+ * @version $Revision$ $Date$
+ * @since 1.0
+ *
+ */
+@Aspect
+public final class StatisticManagementAspect {
+	
+	/** The list of StatisticManagers to use to update statistics. */
+	@NotNull
+	private final List<StatisticManager> statisticManagers;
+	
+	/** The default application code. */
+	@NotNull
+	private final String applicationCode;
+	
+	@NotNull
+	private ClientInfoResolver clientInfoResolver = new DefaultClientInfoResolver();
+	
+	@NotEmpty
+	private Map<Class <? extends StatisticNameResolver>, StatisticNameResolver> nameResolvers = new HashMap<Class <? extends StatisticNameResolver>, StatisticNameResolver>();
+	
+	public StatisticManagementAspect(final List<StatisticManager> statisticManagers, final String applicationCode) {
+		this.statisticManagers = statisticManagers;
+		this.applicationCode = applicationCode;
+		
+		nameResolvers.put(DefaultStatisticNameResolver.class, new DefaultStatisticNameResolver());
+	}
+	
+    @Around(value="@annotation(statistic)", argNames="statistic")
+    public Object handleStatisticGathering(final ProceedingJoinPoint joinPoint, final Statistic statistic) throws Throwable {
+    	Object retVal = null;
+    	String name = null;
+    	try {
+    		retVal = joinPoint.proceed();
+    		name = nameResolvers.get(statistic.nameResolverClass()).resolveFrom(joinPoint, retVal, statistic);
+    		return retVal;
+    	} catch (final Exception e) {
+    		name = nameResolvers.get(statistic.nameResolverClass()).resolveFrom(joinPoint, e, statistic);
+    		throw e;
+    	} finally {
+    		final ClientInfo clientInfo = this.clientInfoResolver.resolveFrom(joinPoint, retVal);
+    		final String appCode = (statistic.applicationCode() !=null && statistic.applicationCode().length() > 0) ? statistic.applicationCode() : this.applicationCode;
+	    	final StatisticActionContext statisticActionContext = new StatisticActionContext(new Date(), name, statistic.requiredPrecision(), clientInfo.getServerIpAddress(), appCode);
+	    	for (final StatisticManager manager : this.statisticManagers) {
+	    		manager.recalculate(statisticActionContext);
+	    	}
+    	}
+    }
+
+    public void setClientInfoResolver(final ClientInfoResolver clientInfoResolver) {
+		this.clientInfoResolver = clientInfoResolver;
+	}
+    
+    public void setAdditionalStatisticNameResolvers(final List<StatisticNameResolver> statisticNameResolvers) {
+    	for (final StatisticNameResolver resolver : statisticNameResolvers) {
+    		this.nameResolvers.put(resolver.getClass(), resolver);
+    	}
+    }
+}
---
+++ inspektr-support-spring/src/main/java/org/inspektr/support/spring/ParameterValidationAnnotationAspect.java
@@ -0,0 +1,77 @@
+/**
+ *  Copyright 2007 Rutgers, the State University of New Jersey
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *      
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.inspektr.support.spring;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.inspektr.common.annotation.GreaterThan;
+import org.inspektr.common.annotation.IsIn;
+import org.inspektr.common.annotation.NotEmpty;
+import org.inspektr.common.annotation.NotNull;
+import org.inspektr.common.validation.AnnotationValidator;
+import org.inspektr.common.validation.GreaterThanAnnotationValidator;
+import org.inspektr.common.validation.IsInAnnotationValidator;
+import org.inspektr.common.validation.NotEmptyAnnotationValidator;
+import org.inspektr.common.validation.NotNullAnnotationValidator;
+
+/**
+ * Aspect that will read the parameters for the method and validate them against the known validators.
+ * 
+ * @author Scott Battaglia
+ * @version $Revision$ $Date$
+ * @since 1.0
+ *
+ */
+@Aspect
+public final class ParameterValidationAnnotationAspect {
+	
+	private final Map<Class<? extends Annotation>, AnnotationValidator> annotationMappings = new HashMap<Class<? extends Annotation>, AnnotationValidator>();
+	
+	public ParameterValidationAnnotationAspect() {
+    	this.annotationMappings.put(GreaterThan.class, new GreaterThanAnnotationValidator());
+    	this.annotationMappings.put(IsIn.class, new IsInAnnotationValidator());
+    	this.annotationMappings.put(NotEmpty.class, new NotEmptyAnnotationValidator());
+    	this.annotationMappings.put(NotNull.class, new NotNullAnnotationValidator());
+	}
+	
+	@Before(value="")
+	public void doValidationCheck(final JoinPoint joinPoint) {
+		final Object[] args = joinPoint.getArgs();
+		final MethodSignature m = (MethodSignature) joinPoint.getStaticPart().getSignature();
+		final Method method = m.getMethod();
+		
+		final Annotation[][] annotations = method.getParameterAnnotations();
+		
+		for (int i = 0; i < annotations.length; i++) {
+			final Annotation[] annotationsForParam = annotations[i];
+			for (int j = 0; j < annotationsForParam.length; i++) {
+				final Annotation annotation = annotationsForParam[j];
+				final AnnotationValidator validator = annotationMappings.get(annotation.getClass());
+				
+				if (validator != null) {
+					validator.validate(method, annotation, args[i], i);
+				}
+			}
+		}	
+	}
+}
